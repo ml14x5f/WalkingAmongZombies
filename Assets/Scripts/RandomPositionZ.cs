7cdda433e91cd1f5e87f5085d7d35ec42ff99bba
Xinyu Fan <ml14x5f@leeds.ac.uk>
2018-04-29 22:41:42 +0100

    add zombies and update controller

---
+++ Assets/Scripts/RandomPositionZ.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class RandomPositionZ : MonoBehaviour {
+
+	void Start () {
+        StartCoroutine(RePositionWithDelay());
+	}
+
+    IEnumerator RePositionWithDelay()
+    {
+        while (true)
+        {
+            // change random position each 3 seconds
+            // this is for keep zombie walking
+            SetRandomPosition();
+            yield return new WaitForSeconds(3);
+        }
+    }
+
+    void SetRandomPosition()
+    {
+        // Define area
+        float x = Random.Range(-1.0f, 19.0f);
+        float z = Random.Range(-13.0f, 7.0f);
+
+        // print x and z, and keep two bits after a decimal point
+        Debug.Log("X, Z: " + x.ToString("F2") + ", " + z.ToString("F2"));
+
+        // we set y = 0 to make sure object is on the ground
+        transform.position = new Vector3(x, 0.0f, z);
+    }
+}
---
+++ Assets/Scripts/hand.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+
+public class hand : MonoBehaviour {
+
+    public GameObject torchPrefab;  // a reference to the torch’s prefab
+    private GameObject torch;   // torch stores a reference to an instance of the torch
+
+    private SteamVR_TrackedObject trackedObj;
+
+    private SteamVR_Controller.Device Controller
+    {
+        get { return SteamVR_Controller.Input((int)trackedObj.index); }
+    }
+
+    void Awake()
+    {
+        trackedObj = GetComponent<SteamVR_TrackedObject>(); // get a reference to the SteamVR_TrackedObject component that’s attached to the controllers
+    }
+
+    private void Showtorch()
+    {
+        torch.SetActive(true);
+    }
+
+    // new
+    void Start()
+    {
+        torch = Instantiate(torchPrefab);
+    }
+
+    void Update ()
+    {
+		if (Controller.GetHairTriggerDown())
+        {
+            Showtorch();    // show the torch when press trigger
+        }
+        else
+        {
+            torch.SetActive(false); // disappear when release trigger
+        }
+  
+    }
+}
